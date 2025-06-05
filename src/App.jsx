2d44ffb55e7502db9fdd1b993ee484299b8f0d22
RohanTewari <rohantewari2009@gmail.com>
2025-06-05 16:13:13 -0400

    Added a button module and learnt module and inlline css styling for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Card from './Card.jsx';
+import Button from './Button.jsx';
 
 function App() {
   return(
     <>
-      <Card />
-      <Card />
+      <Button />
     </>
   );
 }
---
+++ src/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function Button(){
+
+const styles = {
+    backgroundColor: "hsl(0, 100.00%, 50.00%)",
+    color:"white",
+    padding: "10px 20px",
+    borderradius: "5px",
+    border: "none",
+    cursor: "pointer",
+}
+
+    return(<button style ={styles}>click me</button>);
+
+}
+
+    
+
+
+export default Button
